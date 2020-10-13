d398c1f4840df2579cdc64230de6610acda79265
chipfody <60615532+chipfody@users.noreply.github.com>
2020-10-13 17:08:59 -0400

    added souvenier properties

--- src/main/java/models/souvenier.java
+++ src/main/java/models/souvenier.java
@@ -1,4 +1,12 @@
 package models;
 
 public class souvenier {
+    private int id;
+    private int quantity;
+    private String spoons;
+    private String snowGlobes;
+    private String t_shirts;
+    private String dolls;
+    private float price;
+
 }
