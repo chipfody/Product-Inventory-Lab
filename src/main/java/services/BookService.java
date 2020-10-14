2293eaedc1fed4f4088e0f74f11f91992e60ce7f
chipfody <60615532+chipfody@users.noreply.github.com>
2020-10-14 17:10:16 -0400

    working on book service tests

--- src/main/java/services/BookService.java
+++ src/main/java/services/BookService.java
@@ -8,7 +8,6 @@ import java.util.List;
 public class BookService {
 
     private static int nextBookId = 1;
-
     private List<Book> bookList = new ArrayList<>();
 
     public Book create(int isbn, int quantity, String title, String author, String genre, float price) {
@@ -17,4 +16,27 @@ public class BookService {
 
     return newBook;
     }
+
+    public Book findBook(int bookId) {
+        Book foundBook = null;
+
+        for (int i = 0; i < bookList.size(); i++) {
+            System.out.println(bookList.get(i).getTitle());
+            if (i == bookId) {
+                foundBook = bookList.get(i);
+                break;
+            }
+        }
+        return foundBook;
+    }
+
+
+    public Book[] findAll() {
+        Book[] library = bookList.toArray(new Book[0]);
+        for (int i = 0; i < bookList.size(); i++) {
+            library[i] = bookList.get(i);
+        }
+        return library;
+    }
+
 }
--- src/test/java/services/BookServiceTest.java
+++ src/test/java/services/BookServiceTest.java
@@ -2,35 +2,54 @@ package services;
 
 import junit.framework.Assert;
 import org.junit.Test;
-
 import models.Book;
 
+
 public class BookServiceTest {
+
+    //given
+    int expectedBookId = 1;
+    int expectedIsbn = 125711;
+    int expectedQuantity = 154;
+    String expectedTitle = "Atlas Shrugged";
+    String expectedAuthor = "Ayn Rand";
+    String expectedGenre = "classic";
+    float expectedPrice = 29.99f;
+
+    int expectedBookId2 = 2;
+    int expectedIsbn2 = 38601;
+    int expectedQuantity2 = 172;
+    String expectedTitle2 = "It";
+    String expectedAuthor2 = "Stephen King";
+    String expectedGenre2 = "horror";
+    float expectedPrice2 = 19.99f;
+
+    int expectedBookId3 = 3;
+    int expectedIsbn3 = 112246;
+    int expectedQuantity3 = 72;
+    String expectedTitle3 = "A Chain of Thunder";
+    String expectedAuthor3 = "Jeff Shaar";
+    String expectedGenre3 = "historical fiction";
+    float expectedPrice3 = 24.99f;
+
+    BookService bookService = new BookService();
+
+
 
     @Test
     public void bookServiceCreateTest() {
 
-        //given
-        int expectedBookId = 1;
-        int expectedIsbn = 125711;
-        int expectedQuantity = 154;
-        String expectedTitle = "Atlas Shrugged";
-        String expectedAuthor = "Ayn Rand";
-        String expectedGenre = "classic";
-        float expectedPrice = 29.99f;
-
         //create test model
-        BookService bookService = new BookService();
-        Book testBook2 = bookService.create(expectedIsbn, expectedQuantity, expectedTitle, expectedAuthor, expectedGenre, expectedPrice);
+        Book testBook = bookService.create(expectedIsbn, expectedQuantity, expectedTitle, expectedAuthor, expectedGenre, expectedPrice);
 
         //collect data from new book
-        int actualBookId = testBook2.getBookId();
-        int actualIsbn = testBook2.getIsbn();
-        int actualQuantity = testBook2.getQuantity();
-        String actualTitle = testBook2.getTitle();
-        String actualAuthor = testBook2.getAuthor();
-        String actualGenre = testBook2.getGenre();
-        float actualPrice = testBook2.getPrice();
+        int actualBookId = testBook.getBookId();
+        int actualIsbn = testBook.getIsbn();
+        int actualQuantity = testBook.getQuantity();
+        String actualTitle = testBook.getTitle();
+        String actualAuthor = testBook.getAuthor();
+        String actualGenre = testBook.getGenre();
+        float actualPrice = testBook.getPrice();
 
         //then
         Assert.assertEquals(expectedBookId, actualBookId);
@@ -43,5 +62,22 @@ public class BookServiceTest {
     }
 
     @Test
-    public void
+    public void bookServiceFindByIdTest() {
+
+        //create new book model
+        Book testBook = bookService.create(expectedIsbn, expectedQuantity, expectedTitle, expectedAuthor, expectedGenre, expectedPrice);
+        Book testBook2 = bookService.create(expectedIsbn2, expectedQuantity2, expectedTitle2, expectedAuthor2, expectedGenre2, expectedPrice2);
+
+        //when
+        String actualTitle = testBook2.getTitle();
+
+
+        //then
+        Assert.assertEquals(Integer.class.getName(), new Integer(expectedBookId2).getClass().getName());
+        Assert.assertEquals(actualTitle, bookService.findBook(expectedBookId2));
+    }
+    @Test
+    public void findAllTest() {
+        System.out.println(bookService.findAll());
+    }
 }
